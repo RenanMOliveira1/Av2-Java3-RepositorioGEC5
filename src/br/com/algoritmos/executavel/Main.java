6262b96fd050c3d6ba05d44112d8d3b95777e19e
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-12-11 15:14:23 -0200

    Merge Sort v1.5

--- src/br/com/algoritmos/executavel/Main.java
+++ src/br/com/algoritmos/executavel/Main.java
@@ -3,8 +3,11 @@ package br.com.algoritmos.executavel;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.sun.scenario.effect.Merge;
+
 import br.com.algoritmos.solucao.BuscaBinaria;
 import br.com.algoritmos.solucao.InsertionSort;
+import br.com.algoritmos.solucao.MergeSort;
 import br.com.algoritmos.solucao.QuickSort;
 
 public class Main {
@@ -14,18 +17,28 @@ public class Main {
 		InsertionSort<Integer> insertionSort = new InsertionSort<Integer>("Insertion Sort");
 		QuickSort<Integer> quickSort = new QuickSort<Integer>("Quick Sort");
 		BuscaBinaria<Integer> buscaBinaria = new BuscaBinaria<Integer>();
+		MergeSort<Integer> mergeSort = new MergeSort<Integer>("Merge Sort");
 
 		while(true){
 			ArrayList<Integer> lista = new ArrayList<Integer>();
 			for (long  index = 0; index < 5000; index++) {
 				lista.add(random.nextInt(50000));
 			}
+			
+			System.out.println("Entrada de dados: " + lista.size());
+			mergeSort.getMediaGeral();
+			mergeSort.isOcupado();
+			
+			ArrayList<Integer> listaOrdenadaMerge = mergeSort.ordernarLista(lista);
+			System.out.println("Lista Ordenada por Merge Sort: " + listaOrdenadaMerge.toString());
+			
 			System.out.println("Entrada de dados: " + lista.size());
 			quickSort.getMediaGeral();
 			quickSort.isOcupado();
 
 			ArrayList<Integer> listaOrdenadaQuickSort = quickSort.ordernarLista(lista);
 			System.out.println("listaOrdenada por quicksort"+listaOrdenadaQuickSort.toString());
+			
 
 			System.out.println("Entrada de dados: " + lista.size());
 			insertionSort.getMediaGeral();
--- src/br/com/algoritmos/solucao/MergeSort.java
+++ src/br/com/algoritmos/solucao/MergeSort.java
@@ -2,69 +2,82 @@ package br.com.algoritmos.solucao;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import br.com.algoritmos.ordenacao.IOrdenavel;
 
-public class MergeSort<T extends Comparable<T>> extends Solucao implements IOrdenavel<T>{
-	
+public class MergeSort<T extends Comparable<T>> extends Solucao implements IOrdenavel<T> {
+
 	ArrayList<T> listaDesordenada;
-	
-	protected MergeSort(String _nomeSolucao) {
-		super(_nomeSolucao);	
+
+	public MergeSort(String _nomeSolucao) {
+		super(_nomeSolucao);
 	}
 
 	public ArrayList<T> ordernarLista(ArrayList<T> lista) {
 		Date dataInicial = new Date();
 		setOcupado(true);
 		listaDesordenada = lista;
-		sort(listaDesordenada);
+		listaDesordenada = mergeSort(listaDesordenada);
 		setOcupado(false);
 		Date dataFinal = new Date();
 		adicionarTempoDuracao(dataInicial, dataFinal);
 
 		return listaDesordenada;
 	}
-	
-	public void sort(ArrayList<T> lista) {
-        doMergeSort(0, lista.size() - 1);
-    }
- 
-    private void doMergeSort(int lowerIndex, int higherIndex) {
-         
-        if (lowerIndex < higherIndex) {
-            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
-            // Below step sorts the left side of the array
-            doMergeSort(lowerIndex, middle);
-            // Below step sorts the right side of the array
-            doMergeSort(middle + 1, higherIndex);
-            // Now merge both sides
-            mergeParts(lowerIndex, middle, higherIndex);
-        }
-    }
- 
-    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
- 
-    	ArrayList<T> listaTemporaria = new ArrayList<T>();
-    	
-        int menorIndice = lowerIndex;
-        int indiceMeio = middle + 1;
-        int menorIndice2 = lowerIndex;
-        while (menorIndice <= middle && indiceMeio <= higherIndex) {
-            if (listaTemporaria.get(menorIndice).compareTo(listaTemporaria.get(indiceMeio)) <= 0) {
-            	listaDesordenada.add(menorIndice2, listaTemporaria.get(menorIndice));
-                menorIndice++;
-            } else {
-            	listaDesordenada.add(menorIndice2, listaTemporaria.get(indiceMeio));
-                indiceMeio++;
-            }
-            menorIndice2++;
-        }
-        while (menorIndice <= middle) {
-            listaDesordenada.add(menorIndice2, listaTemporaria.get(menorIndice));
-            menorIndice2++;
-            menorIndice++;
-        }
- 
-    }
+
+	public static <T extends Comparable<T>> ArrayList<T> mergeSort(List<T> m) {
+		
+		if (m.size() <= 1)
+			return new ArrayList<T>(m);
+
+		int middle = m.size() / 2;
+		List<T> left = m.subList(0, middle);
+		List<T> right = m.subList(middle, m.size());
+
+		right = mergeSort(right);
+		left = mergeSort(left);
+		List<T> result = merge(left, right);
+		
+		return new ArrayList<T>(result);
+	}
+
+	public static <T extends Comparable<T>> ArrayList<T> merge(List<T> left, List<T> right) {
+		ArrayList<T> result = new ArrayList<T>();
+		Iterator<T> it1 = left.iterator();
+		Iterator<T> it2 = right.iterator();
+
+		T x = it1.next();
+		T y = it2.next();
+		while (true) {
+			// change the direction of this comparison to change the direction
+			// of the sort
+			if (x.compareTo(y) <= 0) {
+				result.add(x);
+				if (it1.hasNext()) {
+					x = it1.next();
+				} else {
+					result.add(y);
+					while (it2.hasNext()) {
+						result.add(it2.next());
+					}
+					break;
+				}
+			} else {
+				result.add(y);
+				if (it2.hasNext()) {
+					y = it2.next();
+				} else {
+					result.add(x);
+					while (it1.hasNext()) {
+						result.add(it1.next());
+					}
+					break;
+				}
+			}
+		}
+		return result;
+	}
 
 }
